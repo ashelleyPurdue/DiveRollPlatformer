9dc6f6d04ac66b6cb331d4c0b45cea7a3a3c9152
Alex Shelley <alexthepeppershelley@gmail.com>
2022-07-04 17:22:47 -0400

    Add jump button to InputService

--- Scripts/Services/InputService.cs
+++ Scripts/Services/InputService.cs
@@ -5,6 +5,10 @@ namespace DiveRollPlatformer
     public interface IInputService
     {
         Vector2 RightStick {get;}
+
+        bool JumpHeld {get;}
+        bool JumpPressed {get;}
+        bool JumpReleased {get;}
     }
 
     public class InputService : IInputService
@@ -15,5 +19,9 @@ namespace DiveRollPlatformer
             "RightStickDown",
             "RightStickUp"
         );
+
+        public bool JumpHeld => Input.IsActionPressed("Jump");
+        public bool JumpPressed => Input.IsActionJustPressed("Jump");
+        public bool JumpReleased => Input.IsActionJustReleased("Jump");
     }
 }
