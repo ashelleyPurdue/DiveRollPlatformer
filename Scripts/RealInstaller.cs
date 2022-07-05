dbf0e6da7a72ba31ea6d2c40a754252665448289
Alex Shelley <alexthepeppershelley@gmail.com>
2022-07-05 13:13:21 -0400

    Add TimeService

--- Scripts/RealInstaller.cs
+++ Scripts/RealInstaller.cs
@@ -1,12 +1,24 @@
+using SimpleInjector;
 using DiveRollPlatformer.DependencyInjection;
 
 namespace DiveRollPlatformer
 {
     public class RealInstaller : SiblingNodeInstaller
     {
-        protected override void RegisterBindings(SimpleInjector.Container container)
+        protected override void RegisterBindings(Container container)
         {
             container.Register<IInputService, InputService>();
+
+            RegisterNode<ITimeService, TimeService>(container);
+        }
+
+        private void RegisterNode<TService, TNode>(Container container)
+            where TService : class
+            where TNode : Godot.Node, TService, new()
+        {
+            var node = new TNode();
+            AddChild(node);
+            container.RegisterInstance<TService>(node);
         }
     }
 }
