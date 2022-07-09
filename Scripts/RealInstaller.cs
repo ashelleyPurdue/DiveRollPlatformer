ebfd6a6b23db41258328408f60a70062e93c71ff
Alex Shelley <alexthepeppershelley@gmail.com>
2022-07-09 14:59:52 -0400

    Add RegisterSceneSingleton

--- Scripts/RealInstaller.cs
+++ Scripts/RealInstaller.cs
@@ -1,3 +1,5 @@
+using System;
+using Godot;
 using SimpleInjector;
 using DiveRollPlatformer.DependencyInjection;
 
@@ -19,6 +21,25 @@ namespace DiveRollPlatformer
             var node = new TNode();
             AddChild(node);
             Container.RegisterInstance<TService>(node);
+        }
+
+        private void RegisterSceneSingleton<TService>(string scenePath)
+            where TService : class
+        {
+            Container.RegisterSingleton<TService>(Factory);
+
+            TService Factory()
+            {
+                var node = GD.Load<PackedScene>(scenePath).Instance();
+
+                if (!(node is TService s))
+                    throw new Exception($"The root node of {scenePath} does not implement {typeof(TService).Name}");
+
+                Utils.InitializeNode(node, Container);
+
+                AddChild(node);
+                return s;
+            }
         }
     }
 }
